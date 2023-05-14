0bbb66e2b4945032c876455a00bf1fbb608c7176
justin <pansa@localhost.localdomain>
2023-05-14 12:52:36 -0400

    fixed typo 5/14/23

--- src/components/Content1.js
+++ src/components/Content1.js
@@ -3,8 +3,8 @@ import React from 'react'
 const Content1 = () => {
   return (
     <div>
-      <center>This is a prodject to help me learn AWS and practice a little javascript.<br/></center>
-      <center>It queries ebay using the ebay API, so you're probably better off just using the EBay website.</center>
+      <center>This is a project to help me learn AWS and practice a little javascript.<br/></center>
+      <center>It queries ebay using the ebay API, so you're probably better off just using the ebay website.</center>
     </div>
   )
 }
