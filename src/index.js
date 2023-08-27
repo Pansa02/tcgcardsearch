361816f801eabb8fe62c1f5e3c551d71ed7b6cd6
Pansa02 <jalmeida88@outlook.com>
2023-08-26 21:49:51 -0400

    commented out ./aws-exports in index.js file for jenkins test

--- src/index.js
+++ src/index.js
@@ -5,14 +5,14 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 import { ThemeProvider } from "@aws-amplify/ui-react";
-import { Amplify, API } from 'aws-amplify';
+// import { Amplify, API } from 'aws-amplify';
 
-import awsconfig from './aws-exports';
+// import awsconfig from './aws-exports';
 
 import "@aws-amplify/ui-react/styles.css";
 import { studioTheme } from "./ui-components";
 
-Amplify.configure(awsconfig);
+// Amplify.configure(awsconfig);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
